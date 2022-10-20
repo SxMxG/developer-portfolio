062bc81b3b126d1fdad97d104b55a8995470a100
SxMxG <sxmxg01@gmail.com>
2022-10-20 03:24:22 +0000

    Main start of thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,5 @@
-<script context="module">
-  import { gql, GraphQLClient } from 'graphql-request'
-
-  export const load = async () => {
-    const client = new GraphQLClient(
-      import.meta.env.VITE_GRAPHQL_API
-    )
-
-    const query = gql`
-      query GetProjects {
-      projects {
-        name
-        slug
-        description
-        image {
-          url
-        }
-      }
-    }
-    `
-
-    const { projects } = await client.request(query)
-
-    return {
-      props: {
-        projects,
-      },
-    }
-  }
-</script>
-
 <script>
-  export let projects
+  export let data;
 </script>
 
-<pre>{JSON.stringify(projects, null, 2)}</pre>
+<pre>{JSON.stringify(data.projects, null, 2)}</pre>
